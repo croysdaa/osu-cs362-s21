fce25f9ba6e86a1dcf6e761ccb2698a82dba445c
croysdaa <80421676+croysdaa@users.noreply.github.com>
2021-04-16 23:41:13 -0700

    Update password.py

--- password.py
+++ password.py
@@ -1,1 +1,8 @@
-
+import random
+usernum = input("Enter a number: ")
+i = 1
+password = random.randint(1,10)
+while i < int(usernum):
+    password = str(password) + str(random.randint(1,10))
+    i = i + 1
+print(password)
