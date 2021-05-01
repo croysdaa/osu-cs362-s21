7ed64c3b797a96760c2ee4572938b24cc8b1b216
croysdaa <80421676+croysdaa@users.noreply.github.com>
2021-04-30 22:50:46 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,15 +9,19 @@ while answer != 'quit':
         print("Goodbye")
     elif answer == 'a':
         sum1 = float(first) + float(sec)
+        assertEquals(first+sec,sum1)
         print(sum1)
     elif answer == 's':
         difference = float(first) - float(sec)
+        assertEquals(first-sec,difference)
         print(difference)
     elif answer == 'm':
         product = float(first) * float(sec)
+        assertEquals(first*sec,product)
         print(product)
     elif answer == 'd':
         quotient = float(first) / float(sec)
+        assertEquals(first/sec,quotient)
         print(quotient)
     else:
         print("Try again")
