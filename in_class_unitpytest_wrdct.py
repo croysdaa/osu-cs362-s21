90ed7ca7c9b40e7533fa84fe77c6d3014c4fb00d
croysdaa <80421676+croysdaa@users.noreply.github.com>
2021-05-17 00:22:32 -0700

    Update in_class_unitpytest_wrdct.py

--- in_class_unitpytest_wrdct.py
+++ in_class_unitpytest_wrdct.py
@@ -1,3 +1,8 @@
+import pytest
+
 userInput = input("Enter a string to check its word count: ")
 wordCount = len(userInput.split())
 print ("Your sentence has: " +  str(wordCount) + " words.")
+
+#def test_capital_case():
+#    assert capital_case('semaphore') == 'Semaphore'
