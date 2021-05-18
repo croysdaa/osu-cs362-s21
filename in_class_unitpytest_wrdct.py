eff0478f1c89128a71026ba8f55f2f1d89f02b37
croysdaa <80421676+croysdaa@users.noreply.github.com>
2021-05-17 23:34:48 -0700

    Update in_class_unitpytest_wrdct.py

--- in_class_unitpytest_wrdct.py
+++ in_class_unitpytest_wrdct.py
@@ -1,8 +1,28 @@
+import re
 import pytest
+import unittest
 
 userInput = input("Enter a string to check its word count: ")
-wordCount = len(userInput.split())
-print ("Your sentence has: " +  str(wordCount) + " words.")
 
-#def test_capital_case():
-#    assert capital_case('semaphore') == 'Semaphore'
+def findWordCount(userInput):
+    wordCount = len(re.split(';|.|,|\*|\n|\t', userInput))
+    print ("Your sentence has " +  str(wordCount) + " words.")
+    return(wordCount)
+
+def test_enter():
+    assert findWordCount('hi\nthere') == 2
+    
+def test_tab():
+    assert findWordCount('hi\tthere') == 2
+    
+def test_astrix():
+    assert findWordCount('hi\*there') == 2
+    
+def test_comma():
+    assert findWordCount('hi,there') == 2
+    
+def test_semi():
+    assert findWordCount('hi;there') == 2
+    
+def test_period():
+    assert findWordCount('hi.there') == 2
